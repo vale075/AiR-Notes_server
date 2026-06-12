e4df2b70d261ba1e5613c1329f64249ff94c0dc3
vale075 <63117317+vale075@users.noreply.github.com>
2026-06-12 15:10:34 +0000

    Correction de l'accès aux medias lors du dev

--- AiR_Notes_server/urls.py
+++ AiR_Notes_server/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.utils.html import format_html
@@ -39,6 +40,26 @@ urlpatterns = [
     path("api/", api.urls),
     path("", include("notes.urls")),
 ]
+
+# Made with ChatGPT to serve Media files even when debug=False
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    from django.urls import re_path
+    from django.views.static import serve
+
+    urlpatterns += [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {
+                "document_root": settings.MEDIA_ROOT,
+            },
+        ),
+    ]
+# By serving media files via Django directly, we loose performance. But we cannot serve
+# them with whitenoise, so serving them outside of Django would necessitate a nginx
+# server.
 
 if settings.DEBUG and not settings.MINIMAL:
     # Include django_browser_reload URLs only in DEBUG mode
